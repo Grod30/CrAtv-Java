b29cbdfe5335dbae139ac9a2e7592d719a981ef0
Grod30 <girodu97@gmail.com>
2023-07-27 15:26:57 -0600

    config.properties added

---
+++ src/main/java/connector/Configuration.java
@@ -0,0 +1,11 @@
+package connector;
+
+public class Configuration {
+    private String api_username;
+    private String api_password;
+
+    private String key_path;
+    private String key_pin;
+
+
+}
